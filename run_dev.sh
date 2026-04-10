706d97ea6ab614671d0ee8c4ea41502847e9df69
Simone Freddio <simone.freddio@voippartners.it>
2026-04-10 16:45:26 +0200

    ...

--- run_dev.sh
+++ run_dev.sh
@@ -5,10 +5,10 @@ PROJECT_DIR="${HOME}/Documenti/progetti/digital_frame"
 VENV_PYTHON="${PROJECT_DIR}/.venv/bin/python"
 
 export KIVY_NO_ARGS=1
-export KIVY_NO_CONSOLELOG=0
-export KIVY_LOG_LEVEL=info
-export KIVY_WINDOW=auto
+export KIVY_LOG_LEVEL=debug
+export KIVY_WINDOW=egl_rpi
 export KIVY_GL_BACKEND=gl
+export KIVY_BCM_DISPMANX_ID=2
 
 cd "${PROJECT_DIR}"
 
